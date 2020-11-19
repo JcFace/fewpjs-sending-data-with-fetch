6a3bca0ac3ef52e79cf4e75b3e6491bdaadec971
JcFace <jcv@gmail.com>
2020-11-19 10:16:42 -0800

    commit tree 022df1398b6508153bd60d8f591dff25d31fc88d

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // Add your code here
+function submitData(name, email) { 
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  };
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      appendToDom(object.id);
+    })
+    .catch(function(error) {
+      errorAppend(error);
+      console.log(error.message);
+    });
+}
+
+function appendToDom(id) {
+  let body = document.querySelector('body')
+  body.innerHTML = id
+}
+
+function errorAppend(error) {
+  let body = document.querySelector('body')
+  body.innerHTML = error.message
+}
+
+submitData('Jessse', '1234@gmail.com')
+
+
+
+
+//     fetch('http://localhost:3000/users', configObj)
+//       .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(object) {
+//         console.log(object);
+//       })
+//       .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//       });
+// };
+
+
+
+
+// let configObj = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         name: name,
+//         email: email
+//       })
+// };
+
+    
+   
+  
+//   fetch("http://localhost:3000/users", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
